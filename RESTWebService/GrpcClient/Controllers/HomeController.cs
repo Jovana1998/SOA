67311a35df48aa369c9bc76cfbc9a45018d07a68
JovanaJovic98 <jocaboca1998@gmail.com>
2023-04-23 18:57:35 +0200

    Dodat grpc projekat

---
+++ RESTWebService/GrpcClient/Controllers/HomeController.cs
@@ -0,0 +1,43 @@
+﻿using Grpc.Net.Client;
+using GrpcClient;
+using GrpcService;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace GrpcClient.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var httpHandler = new HttpClientHandler();
+            httpHandler.ServerCertificateCustomValidationCallback =
+    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+
+            var channel = GrpcChannel.ForAddress("http://localhost:5001",
+    new GrpcChannelOptions { HttpHandler = httpHandler });
+            var client = new Greeter.GreeterClient(channel);
+            var request = new HelloRequest { Name = "Joka"};
+            var reply = await client.SayHelloAsync(request);
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
