ae50f4fc38adbf8b5734a09891fafc3dfd7d48c8
JovanaJovic <jovana.jovic@profitoptics.com>
2023-05-16 19:48:05 +0200

    Finished grpc service

--- GrpcServiceProject/GrpcClientProject/Controllers/HomeController.cs
+++ GrpcServiceProject/GrpcClientProject/Controllers/HomeController.cs
@@ -1,6 +1,4 @@
 using Grpc.Net.Client;
-using GrpcClient;
-using GrpcService;
 using Microsoft.AspNetCore.Mvc;
 using System.ComponentModel.DataAnnotations;
 using System.Diagnostics;
@@ -13,28 +11,52 @@ namespace GrpcClient.Controllers
     public class HomeController : ControllerBase
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly Temperature.TemperatureClient client;
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
-        }
-        [HttpGet]
-        public async Task<IActionResult> Index()
-        {
             var httpHandler = new HttpClientHandler();
             httpHandler.ServerCertificateCustomValidationCallback =
-    HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
+            HttpClientHandler.DangerousAcceptAnyServerCertificateValidator;
 
             var channel = GrpcChannel.ForAddress("http://localhost:5001",
-    new GrpcChannelOptions { HttpHandler = httpHandler });
-            var client = new Temperature.TemperatureClient(channel);
-            var request = new HelloRequest { Name = "Joka" };
-            var reply = await client.SayHelloAsync(request);
-            return Ok();
+            new GrpcChannelOptions { HttpHandler = httpHandler });
+            client = new Temperature.TemperatureClient(channel);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetAllData()
+        {
+            var reply = client.GetAllData(new Empty());
+            return Ok(reply);
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetById([FromQuery]GetByIdRequest request)
+        {
+            var reply = client.GetById(request);
+            return Ok(reply);
+        }
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateRequest request)
+        {
+            var reply = client.Create(request);
+            return Ok(reply);
+        }
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateRequest request)
+        {
+            var reply = client.Update(request);
+            return Ok(reply);
+        }
+        [HttpDelete]
+        public async Task<IActionResult> Delete(DeleteRequest request)
+        {
+            var reply = client.Delete(request);
+            return Ok(reply);
         }
 
-       
 
-     
+
+
     }
 }
--- GrpcServiceProject/GrpcService/Services/TemperatureService.cs
+++ GrpcServiceProject/GrpcService/Services/TemperatureService.cs
@@ -2,6 +2,7 @@ using Grpc.Core;
 using GrpcService;
 using GrpcService.Model;
 using MongoDB.Driver;
+using MongoDB.Driver.Core.Operations;
 
 namespace GrpcService.Services
 {
@@ -18,22 +19,102 @@ namespace GrpcService.Services
            
         }
 
-        public override async Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        public override async Task<GetAllDataResponse> GetAllData(Empty request,ServerCallContext context)
         {
-            var res =   await _data.Find(x => x.ID == "6445743c1ea9933fcc9fb205").FirstOrDefaultAsync();
-            return new HelloReply
+            var res = await _data.Find(x => true).ToListAsync();
+            var data = res.Select(x => new Data
             {
-                Message = "Hello " + request.Name
-            };
+                TemperatureC = x.Temperature_c,
+                TimeId = x.Time_id,
+                HumidityP = x.Humidity_p,
+                ID = x.ID,
+                PingMs = x.Ping_ms
+            }).ToList();
+            var response = new GetAllDataResponse();
+        
+            response.Data.AddRange(data); 
+            return response;
         }
-
-        public override async Task<GetAllDataResponse> GetAllData(GetAllDataRequest request, ServerCallContext context)
+        public override async Task<GetByIdResponse> GetById(GetByIdRequest request, ServerCallContext context)
         {
-            var res = await _data?.Find(x => true).ToListAsync();
-            return new GetAllDataResponse
+            var res = await _data.Find(x => true && x.ID == request.Id).FirstOrDefaultAsync();
+
+            var response = new GetByIdResponse();
+            response.Result = res != null ? "Successful" : "There is no data with this id";
+            if(response.Result == "Successful")
             {
-              
+                response.Data = new Data
+                {
+                    TemperatureC = res.Temperature_c,
+                    TimeId = res.Time_id,
+                    HumidityP = res.Humidity_p,
+                    ID = res.ID,
+                    PingMs = res.Ping_ms
+                };
+            }
+            return response;
+        }
+        public override async Task<CreateResponse> Create(CreateRequest request, ServerCallContext context)
+        {
+            var data = new DataModel
+            {
+                Temperature_c = request.TemperatureC,
+                Humidity_p = request.HumidityP,
+                Ping_ms = request.PingMs,
+                Time_id = request.TimeId
             };
+            await _data.InsertOneAsync(data);
+            var result = new CreateResponse();
+            if (data.ID == null)
+            {
+                result.Result = "Error while inserting this data";
+                return result;
+            }
+            result.Data= new Data
+            {
+                TemperatureC = data.Temperature_c,
+                TimeId = data.Time_id,
+                HumidityP = data.Humidity_p,
+                ID = data.ID,
+                PingMs = data.Ping_ms
+            };
+            result.Result = "Successful";
+            return result;
+        }
+        public override async Task<UpdateResponse> Update(UpdateRequest request, ServerCallContext context)
+        {
+            var res = await _data.Find(x => true && x.ID == request.ID).FirstOrDefaultAsync();
+            var result = new UpdateResponse();
+            if (res == null)
+            {
+                result.Result = "There is no data with this Id";
+                return result;
+            }
+            res.Temperature_c = request.TemperatureC;
+            res.Humidity_p = request.HumidityP;
+            res.Ping_ms = request.PingMs;
+            res.Time_id = request.TimeId;
+            await _data.ReplaceOneAsync(x => x.ID == request.ID, res);
+            result.Data= new Data
+            {
+                TemperatureC = res.Temperature_c,
+                TimeId = res.Time_id,
+                HumidityP = res.Humidity_p,
+                ID = res.ID,
+                PingMs = res.Ping_ms
+            };
+            result.Result = "Successful";
+            return result;
+        }
+        public override async Task<DeleteResponse> Delete(DeleteRequest request, ServerCallContext context)
+        {
+            var res = await _data.Find(x => true && x.ID == request.Id).ToListAsync();
+            if(res == null)
+            {
+                return new DeleteResponse { Response = "There is no data with this Id" };
+            }
+           var r= _data.DeleteOneAsync(x => x.ID == request.Id);
+            return new DeleteResponse { Response = "Successful"};
         }
     }
 }
